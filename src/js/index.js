9d5dfd8ff7ad53673b0ff4d89e0c475b1265bb7b
antoniogranadino <110358944+antoniogranadino@users.noreply.github.com>
2023-12-19 20:19:53 +0000

    Pending changes exported from your codespace

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,39 @@
 //import react into the bundle
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+const TrafficLight = () =>{
+    const [ color, setColor] = useState("Red");
+    return(
+        <div className="Container">
+            <div className="Lightpost">
+
+            </div>
+            <div className="Box">
+                    <button
+                     className={`Red ${color === "Red" ? "Light" : " "}`}
+                     onClick={() => setColor("Red")}>    
+                        
+                    </button>
+
+                    <button 
+                    className={`Yellow ${color === "Yellow" ? "Light" : " "}`}
+                    onClick={() => setColor("Yellow")}> 
+                        
+                    </button>
+
+                    <button 
+                    className={`Green ${color === "Green" ? "Light" : " "}`}
+                    onClick={() => setColor("Green")}> 
+                        
+                    </button>
+            </div>
+        </div>
+    )
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
